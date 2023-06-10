1a94c476400ef765612dac550de9fd21c1197f9a
ʀᴀʏ <nn_201312@163.com>
2023-06-10 12:20:42 +0800

    style: fix lint

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -6,6 +6,6 @@ import styles from "./styles/index.module.css";
 
 const App = () => <h1 class={styles.demo}>Retalk</h1>;
 
-
+// eslint-disable-next-line no-console
 console.log(`%c Retalk %c ${packageJSON.version} `, 'background: #006BB8; padding: 5px; border-radius: 3px 0 0 3px; color: #fff', 'background: #006BB818; padding: 5px; border-radius: 0 3px 3px 0; color: #006BB8')
 render(() => <App />, document.querySelector("#retalk")!);
