77a30c30f8538e0a0de158be6a0ce76b22323ca9
Redish101 <jiayunluo@outlook.com>
2023-06-10 11:28:00 +0800

    feat: 对于管理员评论发布的鉴权以及前端版本输出

--- internal/entity/author.go
+++ internal/entity/author.go
@@ -4,6 +4,6 @@ type Author struct {
 	Base
 	Name  string  `json:"name"`
 	Email string  `json:"email"`
-	Link  *string `json:"link"`
+	Link  string `json:"link"`
 	IsAdmin bool `json:"is_admin"`
 }
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,12 @@
 import { render } from "solid-js/web";
+import packageJSON from "../package.json"
+import versionImg from "../assets/version.svg"
 
 import styles from "./styles/index.module.css";
 
+
 const App = () => <h1 class={styles.demo}>Retalk</h1>;
 
+
+console.log(`%c Retalk %c ${packageJSON.version} `, 'background: #006BB8; padding: 5px; border-radius: 3px 0 0 3px; color: #fff', 'background: #006BB818; padding: 5px; border-radius: 0 3px 3px 0; color: #006BB8')
 render(() => <App />, document.querySelector("#retalk")!);
