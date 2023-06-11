d613518edae705c0512b7b03b2d24f24f492dbb0
so1ve <i@mk1.io>
2023-06-11 13:06:54 +0800

    build: fix build

--- ui/uno.config.ts
+++ ui/uno.config.ts
@@ -1,9 +1,11 @@
 import {
   defineConfig,
+  presetUno,
   transformerCompileClass,
   transformerVariantGroup,
 } from "unocss";
 
 export default defineConfig({
+  presets: [presetUno()],
   transformers: [transformerCompileClass(), transformerVariantGroup()],
 });
--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,35 +1,30 @@
 import path from "node:path";
-import { rmdir } from "node:fs/promises";
 
 import { defineConfig } from "vite";
 import Solid from "vite-plugin-solid";
 import Dts from "vite-plugin-dts";
 import Unocss from "unocss/vite";
 
-export default defineConfig(async () => {
-  await rmdir(path.resolve(__dirname, "dist"), { recursive: true });
-
-  return {
-    plugins: [Solid(), Dts(), Unocss()],
-    build: {
-      target: "es2015",
-      outDir: path.resolve(__dirname, "dist"),
-      minify: "esbuild",
-      lib: {
-        entry: path.resolve(__dirname, "src/index.ts"),
-        name: "Retalk",
-        fileName: (format) =>
-          format === "umd"
-            ? "retalk.umd.js"
-            : `retalk.${format === "cjs" ? "c" : "m"}js`,
-        formats: ["umd", "es", "cjs"],
-      },
-      rollupOptions: {
-        output: {
-          assetFileNames: (assetInfo) =>
-            /\.css$/.test(assetInfo.name || "") ? "retalk.css" : "[name].[ext]",
-        },
+export default defineConfig({
+  plugins: [Solid(), Dts(), Unocss()],
+  build: {
+    target: "es2015",
+    outDir: path.resolve(__dirname, "dist"),
+    minify: "esbuild",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "Retalk",
+      fileName: (format) =>
+        format === "umd"
+          ? "retalk.umd.js"
+          : `retalk.${format === "cjs" ? "c" : "m"}js`,
+      formats: ["umd", "es", "cjs"],
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) =>
+          /\.css$/.test(assetInfo.name || "") ? "retalk.css" : "[name].[ext]",
       },
     },
-  };
+  },
 });
