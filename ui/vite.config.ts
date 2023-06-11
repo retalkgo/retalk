b12e41b34d5b90356ad96628c0fe000100829ca1
so1ve <i@mk1.io>
2023-06-11 12:57:17 +0800

    build: improve build

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,31 +1,39 @@
 import path from "node:path";
+import { rmdir } from "node:fs/promises";
 
 import { defineConfig } from "vite";
 import Solid from "vite-plugin-solid";
+import Dts from "vite-plugin-dts";
 
-export default defineConfig({
-  plugins: [Solid()],
-  build: {
-    target: "es2015",
-    outDir: path.resolve(__dirname, "dist"),
-    minify: "esbuild",
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "Retalk",
-      fileName: (format) =>
-        format === "umd" ? "retalk.js" : `retalk.${format}.js`,
-      formats: ["umd", "es", "iife"],
-    },
-    rollupOptions: {
-      output: {
-        assetFileNames: (assetInfo) =>
-          /\.css$/.test(assetInfo.name || "") ? "retalk.css" : "[name].[ext]",
+export default defineConfig(async () => {
+  await rmdir(path.resolve(__dirname, "dist"), { recursive: true });
+
+  return {
+    plugins: [Solid(), Dts()],
+    build: {
+      target: "es2015",
+      outDir: path.resolve(__dirname, "dist"),
+      minify: "esbuild",
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "Retalk",
+        fileName: (format) =>
+          format === "umd"
+            ? "retalk.umd.js"
+            : `retalk.${format === "cjs" ? "c" : "m"}js`,
+        formats: ["umd", "es", "cjs"],
+      },
+      rollupOptions: {
+        output: {
+          assetFileNames: (assetInfo) =>
+            /\.css$/.test(assetInfo.name || "") ? "retalk.css" : "[name].[ext]",
+        },
       },
     },
-  },
-  css: {
-    modules: {
-      generateScopedName: "[hash:6]",
+    css: {
+      modules: {
+        generateScopedName: "[hash:6]",
+      },
     },
-  },
+  };
 });
