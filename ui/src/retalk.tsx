785bc757619b9c0b0b9aa456a3950b18f86641f7
so1ve <i@mk1.io>
2023-06-11 13:10:05 +0800

    feat!: rewrite options

--- ui/src/retalk.tsx
+++ ui/src/retalk.tsx
@@ -1,12 +1,12 @@
 import { render } from "solid-js/web";
 
 import { logRetalkInfo, resolveElement } from "./utils";
-import type { ElementOrSelector, Options } from "./types";
+import type { Options } from "./types";
 
 const App = () => <h1 class=":uno: font-bold">Retalk</h1>;
 
 export default class Retalk {
-  constructor(el: ElementOrSelector, _server: string, options: Options = {}) {
+  constructor({ el, ...options }: Options) {
     const resolvedEl = resolveElement(el);
     if (!resolvedEl) {
       throw new Error(`Retalk: Element ${el as string} not found`);
