7a369984a02906ce72b049c6f625d17467a81e64
Redish101 <jiayunluo@outlook.com>
2023-07-18 21:22:51 +0800

    feat: add turborepo TODO: build error at packages/client

--- packages/client/src/components/Input.tsx
+++ packages/client/src/components/Input.tsx
@@ -6,6 +6,6 @@ export const Input = (props: InputProps) => (
   <input
     {...props}
     type="text"
-    class=":uno: inline-block min-h-8 min-w-0 px-3 click:w-250px inputlike"
+    class=":uno: click:w-250px inline-block min-h-8 min-w-0 px-3 inputlike"
   />
 );
