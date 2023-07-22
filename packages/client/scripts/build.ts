e4fe99d66031f609e59710e5c81eb01e05426034
so1ve <nn_201312@163.com>
2023-07-22 12:44:35 +0800

    fix: build

--- packages/client/scripts/build.ts
+++ packages/client/scripts/build.ts
@@ -3,10 +3,10 @@ import { rm } from "node:fs/promises";
 import { execa } from "execa";
 
 (async () => {
-  await rm("dist", { recursive: true });
-  await execa("pnpm", ["run", "build:real"]);
-  // Cleanup extra files
   try {
+    await rm("dist", { recursive: true });
+    await execa("pnpm", ["run", "build:real"]);
+    // Cleanup extra files
     await rm("dist/components", { recursive: true });
     await rm("dist/index.d.ts");
     await rm("dist/utils.d.ts");
