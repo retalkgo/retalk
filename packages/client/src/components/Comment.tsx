5b4e7768bccc101b2b35609247ded21ad1438156
so1ve <nn_201312@163.com>
2023-07-23 01:24:13 +0800

    chore: add comment props

--- packages/client/src/components/Comment.tsx
+++ packages/client/src/components/Comment.tsx
@@ -1,4 +1,10 @@
-export interface CommentProps {}
+export interface CommentProps {
+	avatar: string;
+	nickname: string;
+	isAdmin: boolean;
+	time: string;
+	content: string;
+}
 
 export const Comment = (_props: CommentProps) => (
 	<div class=":uno: inline-block min-h-9 cursor-pointer rounded-4.5 border-none bg-primary px-7.5 py-2.5 text-3.6 font-semibold text-white transition duration-animation hover:shadow-active" />
