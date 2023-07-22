807b69546475af8d8e22686cf12f7dab46d98d87
so1ve <nn_201312@163.com>
2023-07-22 17:08:40 +0800

    feat(textarea): add props definition

--- packages/client/src/components/Textarea.tsx
+++ packages/client/src/components/Textarea.tsx
@@ -1,3 +1,7 @@
-export const Textarea = () => (
-  <textarea class=":uno: block min-h-20 p-3 w-full inputlike" />
+interface TextareaProps {
+  placeholder?: string;
+}
+
+export const Textarea = (props: TextareaProps) => (
+  <textarea {...props} class=":uno: block min-h-20 p-3 w-full inputlike" />
 );
