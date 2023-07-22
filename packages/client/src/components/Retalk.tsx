f9ef3a336b6b0445f045c835d40c494d76ad6450
so1ve <nn_201312@163.com>
2023-07-22 16:39:24 +0800

    chore: update

--- packages/client/src/components/Retalk.tsx
+++ packages/client/src/components/Retalk.tsx
@@ -3,16 +3,16 @@ import { Input } from "./Input";
 import { Textarea } from "./Textarea";
 
 export default () => (
-  <div class="w-125">
-    <div class="flex gap-3">
+  <div class="uno: w-125 flex flex-col gap-4.5">
+    <div class=":uno: flex gap-3">
       <Input placeholder="昵称" />
       <Input placeholder="邮箱" />
       <Input placeholder="网站" />
     </div>
-    <div class="mt-7.5">
+    <div class=":uno: flex">
       <Textarea />
     </div>
-    <div class="mt-7.5">
+    <div class=":uno: flex justify-end">
       <Button>
         <span>按钮</span>
       </Button>
