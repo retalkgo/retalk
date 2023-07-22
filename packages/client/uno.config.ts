e23dd0e53d1ce0ad353e1a818011d168f4f5a9c1
so1ve <nn_201312@163.com>
2023-07-22 14:31:56 +0800

    fix(textarea): vertical resize only

--- packages/client/uno.config.ts
+++ packages/client/uno.config.ts
@@ -20,7 +20,7 @@ export default defineConfig<Theme>({
   transformers,
   shortcuts: {
     inputlike:
-      "border-2.5 border-solid border-normal hover:(border-primary shadow-active) focus:(border-primary shadow-active) transition duration-animation rounded-4 outline-none",
+      "border-2.5 border-solid border-normal hover:(border-primary shadow-active) focus:(border-primary shadow-active) transition duration-animation rounded-4 outline-none resize-y",
   },
   theme: {
     colors: {
@@ -28,7 +28,7 @@ export default defineConfig<Theme>({
       primary: "#006BB8",
     },
     boxShadow: {
-      active: "0px 4px 16px rgba(0, 107, 184, 0.8);",
+      active: "0px 1px 8px rgba(0, 107, 184, 0.8);",
     },
     duration: {
       animation: "400",
