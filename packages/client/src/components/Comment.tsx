6b8c03c4e397a20afd2ddc98ca3d1e9feb43068e
so1ve <nn_201312@163.com>
2023-07-28 13:52:05 +0800

    chore: update

--- packages/client/src/components/Comment.tsx
+++ packages/client/src/components/Comment.tsx
@@ -1,3 +1,5 @@
+import { Show } from "solid-js";
+
 export interface CommentProps {
 	avatar: string;
 	nickname: string;
@@ -8,11 +10,18 @@ export interface CommentProps {
 
 export const Comment = (props: CommentProps) => (
 	<div class=":uno: flex gap-3">
-		<div class="h-10 w-10 rounded-3 pr-3 shadow-avatar">
+		<div class=":uno: h-10 w-10 rounded-3 pr-3 shadow-avatar">
 			<img src={props.avatar} />
 		</div>
-		<div class="w-full rounded-3 px-8 pb-3 pt-4 text-xs font-600 shadow-comment">
-			<div class="text-primary">{props.nickname}</div>
+		<div class=":uno: w-full rounded-3 px-8 pb-3 pt-4 text-13px text-sm font-600 shadow-comment">
+			<div class=":uno: flex items-center gap-1">
+				<div class=":uno: text-primary">{props.nickname}</div>
+				<Show when={props.isAdmin}>
+					<div class=":uno: scale-80 rounded bg-primary/18 p-1.5 text-xs">
+						<span class=":uno: text-primary">管理员</span>
+					</div>
+				</Show>
+			</div>
 		</div>
 	</div>
 );
---
+++ packages/schema/src/index.ts
@@ -0,0 +1,22 @@
+export interface Author {
+	created_at: string;
+	email: string;
+	id: number;
+	is_admin: boolean;
+	link: string;
+	name: string;
+}
+
+export interface Comment {
+	author: Author;
+	body: string;
+	created_at: String;
+	id: number;
+	path: string;
+}
+
+export interface Result<T = any> {
+	data: T;
+	msg: string;
+	success: boolean;
+}
