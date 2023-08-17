e9b0f015faf94b199470ad1a724be750e197b612
so1ve <i@mk1.io>
2023-08-18 00:36:17 +0800

    fix(api): handle API correctly

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,2 +1,18 @@
-export { HttpClient } from "./swagger";
+import type { ApiConfig } from "./swagger";
+import { Api as BaseApi } from "./swagger";
+
+export class Api<SecurityDataType> extends BaseApi<SecurityDataType> {
+	constructor(apiConfig: ApiConfig<SecurityDataType> = {}) {
+		super();
+		Object.assign(
+			this,
+			Object.assign(apiConfig, {
+				baseApiParams: {
+					...apiConfig.baseApiParams,
+					format: "json",
+				},
+			}),
+		);
+	}
+}
 export * from "./types";
