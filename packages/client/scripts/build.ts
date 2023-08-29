d5eb4b64fe20ca1630abc1ddfba9f525b459df83
so1ve <i@mk1.io>
2023-08-29 15:11:43 +0800

    fix: build

--- packages/client/scripts/build.ts
+++ packages/client/scripts/build.ts
@@ -8,6 +8,8 @@ import { execa } from "execa";
 		await execa("pnpm", ["run", "build:real"], { stdio: "inherit" });
 		// Cleanup extra files
 		await rm("dist/components", { recursive: true });
+		await rm("dist/contexts", { recursive: true });
+		await rm("dist/hooks", { recursive: true });
 		await rm("dist/index.d.ts");
 		await rm("dist/utils.d.ts");
 	} catch {}
