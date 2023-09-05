a8216c4d6f8796792bee1836d3dbc01171bf7d2e
so1ve <i@mk1.io>
2023-09-05 13:10:04 +0800

    fix: types

--- packages/setup/src/App.tsx
+++ packages/setup/src/App.tsx
@@ -1,28 +1,32 @@
-import { Button, TextField, Typography } from "@mui/material";
+import { Button, TextField, Typography, createTheme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import { FormEvent } from "react";
 
-const useStyles = makeStyles((theme) => ({
-	formContainer: {
-		display: "flex",
-		flexDirection: "column",
-		alignItems: "center",
-		padding: theme.spacing(2),
-	},
-	title: {
-		marginBottom: theme.spacing(2),
-	},
-	textField: {
-		marginBottom: theme.spacing(2),
-	},
-	submitButton: {
-		width: "150px",
-	},
-}));
+const useStyles = makeStyles(
+	(theme) => ({
+		formContainer: {
+			display: "flex",
+			flexDirection: "column",
+			alignItems: "center",
+			padding: theme.spacing(2),
+		},
+		title: {
+			marginBottom: theme.spacing(2),
+		},
+		textField: {
+			marginBottom: theme.spacing(2),
+		},
+		submitButton: {
+			width: "150px",
+		},
+	}),
+	{ defaultTheme: createTheme() },
+);
 
 function Form() {
 	const classes = useStyles();
 
-	function handleSubmit(event) {
+	function handleSubmit(event: FormEvent<HTMLFormElement>) {
 		event.preventDefault();
 		// 处理提交逻辑
 	}
