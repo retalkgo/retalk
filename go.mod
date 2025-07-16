b75ebdcb4bf9c754ed176849e99b6c755a549231
Redish101 <i@redish101.top>
2025-07-16 16:58:46 +0800

    feat: 初始化仓库

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/retalkgo/retalk
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/retalkgo/retalk/server"
+
+// retalk server
+// 轻量简洁的评论系统
+// Redish101, 2025-07-16
+func main() {
+	server.Start()
+}
